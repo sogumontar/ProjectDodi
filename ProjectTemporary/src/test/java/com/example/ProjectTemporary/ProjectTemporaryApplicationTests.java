d16ad059794773d70c715bf3f2e56127e32e7dea
unknown <hendrasimz92@gmail.com>
2019-12-17 10:49:49 +0700

    inititalize

---
+++ ProjectTemporary/src/test/java/com/example/ProjectTemporary/ProjectTemporaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectTemporary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTemporaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
